76bea31278cfdb6ca06c08f0bb43c6def784f94a
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2025-09-08 13:33:09 +0200

    Update blog.js

--- bim/routes/blog.js
+++ bim/routes/blog.js
@@ -10,6 +10,7 @@ router.get("/public",blogControllers.getPublic)
 router.get("/:slug", blogControllers.getOne);
 
 // Тільки адмін
+router.get("/", auth, requireRole("admin"), blogControllers.getAll);
 router.post("/", auth, requireRole("admin"), blogControllers.add);
 router.put("/:id", auth, requireRole("admin"), blogControllers.update);
 router.delete("/:id", auth, requireRole("admin"), blogControllers.remove);
